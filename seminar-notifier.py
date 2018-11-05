698bb88e1f276f356bb3fe3e6f31210cde0cbb25
Simon Reich <sreich@physik3.gwdg.de>
2018-11-05 13:33:55 +0100

    Wrapper for class added

---
+++ seminar-notifier.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This file is part of seminar-notifier.
+seminar-notifier is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+seminar-notifier is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with seminar-notifier.    If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# (c) Simon Reich 2018
+
+######################################################
+## No Configuration after this
+######################################################
+
+
+
+from yaml import load
+from bin import seminarnotifier
+
+
+
+def main():
+    # read globalconfig.yaml
+    with open("./globalconfig.yaml", 'r') as stream:
+        config = load(stream)
+ 
+    configSendmail = config["sendmail"]
+    configSeminars = config["seminars"]
+
+    for seminar in configSeminars:
+        seminarnotifier.SeminarNotifier(str(seminar)+"/config", str(seminar)+"/templates", configSendmail)
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
